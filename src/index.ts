cf484edeca42823677dee4d449b529e781ed83a9
DanielLap <daniels.salinas.condor@gmail.com>
2026-08-25 15:41:30 -0600

    correcciones index

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,16 @@
-import { pool } from "./config/db.js";
-import { totalmem } from "node:os";
-import { json } from "node:stream/consumers";
-import { Result } from "pg";
 import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
 import recetasRouter from './routes/recetas.routes.js';
 import { medicosRouter } from "./routes/medicos.routes.js";
 import  citasRouter  from "./routes/citas.routes.js";
+import { 
+  getPatients,
+  getPatientsById,
+  postPatients,
+  putPatientsById,
+  deletePatientByID
+ } from "./controllers/paciente.controller.js";
 dotenv.config();
 
 const app = express();
@@ -15,9 +18,14 @@ app.use(cors());
 app.use(express.json());
 app.use('/recetas', recetasRouter);
 app.use('/citas',citasRouter);
+app.get("/pacientes", getPatients);
+app.get("/pacientes/:id", getPatientsById);
+app.post("/pacientes", postPatients);
+app.put("/pacientes/:id", putPatientsById);
+app.delete("/pacientes/:id", deletePatientByID);
 app.use("/recetas", recetasRouter);
 app.use("/medicos", medicosRouter); //conectamos las rutas de médicos
-
+app.use("/citas", citasRouter);
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
