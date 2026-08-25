331190ea4329753ee706ce76ea59db53a17eb966
natanael <natanaelflorentin3@gmail.com>
2026-08-25 18:08:14 -0300

    fix: conectar swagger-ui en index.ts

--- src/index.ts
+++ src/index.ts
@@ -8,12 +8,11 @@ import {
 } from "./routes/pacientes.routes.js";
 import { pool } from "./config/db.js";
 import type { Request, Response } from "express";
-import { totalmem } from "node:os";
-import { json } from "node:stream/consumers";
-import { Result } from "pg";
 import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
+import fs from "fs";
+import swaggerUi from "swagger-ui-express";
 import recetasRouter from './routes/recetas.routes.js';
 import { medicosRouter } from "./routes/medicos.routes.js";
 import { citasRouter } from "./routes/citas.routes.js";
@@ -23,11 +22,12 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.use(cors());
-
-dotenv.config();
-
-app.use(cors());
 app.use(express.json());
+
+const swaggerFile = JSON.parse(
+  fs.readFileSync(new URL("../swagger-output.json", import.meta.url), "utf-8")
+);
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerFile));
 
 app.get("/pacientes", getpatients);
 app.get("/pacientes/:id", getPatientsById);
